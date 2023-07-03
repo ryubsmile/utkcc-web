7be9c9dd1dca893d21ac86bb4b337440987ecad1
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-07-04 00:54:13 +0900

    feat/navbar implementation complete
    - page.tsx filled with dummy texts

--- utkcc-fe/app/navbar.tsx
+++ utkcc-fe/app/navbar.tsx
@@ -2,14 +2,15 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import './navbar.css';
-import { useState } from 'react';
+import { SetStateAction, useState } from 'react';
 
-function MenuButton() {
-  const [navActive, setNavActive] = useState(false);
+interface navBarProps {
+  getter: boolean;
+  setter: React.Dispatch<SetStateAction<boolean>>;
+}
 
-  if (navActive) {
-  }
+function MenuButton(props: navBarProps) {
+  const [navActive, setNavActive] = [props.getter, props.setter];
 
   return (
     <div
@@ -20,42 +21,70 @@ function MenuButton() {
         console.log(navActive);
       }}
     >
-      <span
-        className={`w-full h-1/6 rounded-md bg-slate-400 ${
-          navActive ? 'menu-stick-1' : ''
+      <Image
+        width={40}
+        height={40}
+        src={`${
+          navActive ? '/assets/menu-opened.png' : '/assets/menu-closed.png'
         }`}
-      ></span>
-      <span
-        className={`w-full h-1/6 rounded-md bg-slate-400 ${
-          navActive ? 'menu-stick-2' : ''
-        }`}
-      ></span>
-      <span
-        className={`w-full h-1/6 rounded-md bg-slate-400 ${
-          navActive ? 'menu-stick-3' : ''
-        }`}
-      ></span>
+        alt="menu"
+        className={`${navActive ? 'scale-100' : 'scale-150'}`}
+      />
     </div>
   );
 }
 
 export default function NavBar({ children }: { children: any }) {
+  const [navActive, setNavActive] = useState(false);
   return (
-    <nav className="w-screen h-14 fixed bg-white border-b border-solid border-transparent select-none">
-      <div className="px-4 py-2 h-full flex items-end">
-        <Link className="w-fit h-full flex gap-2" href="/">
+    <nav
+      className={`w-screen ${
+        navActive ? 'h-screen' : 'h-16'
+      } fixed bg-white border-b border-solid border-transparent font-semibold duration-200 select-none`}
+    >
+      <div className="px-4 py-2 h-16 flex">
+        <Link
+          className={`w-fit h-full flex gap-2 duration-100 items-center ${
+            navActive ? 'opacity-0' : 'opacity-100'
+          }`}
+          href="/"
+        >
           <Image
             src="/logo-nav.png"
             alt="logo"
-            width={35}
-            height={35}
-            className="object-cover"
+            width={36}
+            height={36}
+            className="object-scale-down"
           />
-          <span className="self-end text-xl">UTKCC</span>
+          <span className="text-xl">UTKCC</span>
         </Link>
-        <div className="w-8 h-8 ml-auto py-1">
-          <MenuButton />
+        <div className="w-8 h-8 ml-auto self-center">
+          <MenuButton getter={navActive} setter={setNavActive} />
         </div>
+      </div>
+      <div
+        className={`${
+          navActive ? 'flex' : 'hidden'
+        } flex-col mt-10 pl-16 gap-8 text-xl text-gray-600`}
+      >
+        <Link href="/" onClick={() => setNavActive(false)}>
+          Home
+        </Link>
+        <Link href="/events" onClick={() => setNavActive(false)}>
+          Events
+        </Link>
+        <Link href="/executives" onClick={() => setNavActive(false)}>
+          Executives
+        </Link>
+        <Link href="/newsletter" onClick={() => setNavActive(false)}>
+          Newsletter
+        </Link>
+        <Link href="/resources" onClick={() => setNavActive(false)}>
+          Resources
+        </Link>
+        <Link href="/sponsors" onClick={() => setNavActive(false)}>
+          Sponsors
+        </Link>
       </div>
     </nav>
   );
--- utkcc-fe/app/page.tsx
+++ utkcc-fe/app/page.tsx
@@ -4,16 +4,30 @@ export default function Home() {
   return (
     <div>
       Main page
-      <hr />
-      <Link href="/events">to events</Link>
-      <hr />
-      <Link href="/executives">to executives</Link>
-      <hr />
-      <Link href="/newsletter">to newsletter</Link>
-      <hr />
-      <Link href="/resources">to resources</Link>
-      <hr />
-      <Link href="/sponsors">to sponsors</Link>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit voluptas
+        unde laboriosam error eius ab distinctio fuga facilis, non porro,
+        tempora perferendis aut necessitatibus aspernatur quidem sed corrupti
+        exercitationem voluptates reiciendis inventore culpa et explicabo
+        aperiam ducimus. Obcaecati qui corporis quis in quaerat voluptatem fuga
+        dolorum facilis asperiores ex ducimus animi rem explicabo illum ipsa
+        minima voluptatibus illo, magnam, voluptate blanditiis mollitia alias
+        voluptatum nostrum? Dignissimos tempora modi fugit iure nisi quia atque,
+        obcaecati laudantium possimus earum culpa voluptates incidunt, enim vero
+        esse deserunt perspiciatis, architecto quam. Sit, adipisci aspernatur
+        aliquam velit blanditiis fuga natus laborum repudiandae repellat
+        veritatis libero odio molestias minus dolorem delectus, est rem quisquam
+        reprehenderit facere quam id. Harum perferendis sit quibusdam amet ad
+        exercitationem, velit mollitia atque dolor dignissimos adipisci
+        inventore nisi quia aut incidunt praesentium non ducimus reprehenderit
+        voluptas deserunt minus cumque tempore! Rerum sed, iusto qui quibusdam
+        architecto eius nihil eveniet in sequi culpa quis laudantium ducimus
+        harum quas eligendi corrupti est vero? Labore reiciendis ipsa, sapiente
+        sequi pariatur cupiditate sunt adipisci explicabo quaerat, sed amet.
+        Illum, perferendis eius. Asperiores ratione sit labore mollitia aliquid
+        harum suscipit iste corporis? Eveniet ratione quia nostrum autem ab
+        similique delectus error corporis. Eius vitae est nisi.
+      </div>
     </div>
   );
 }
