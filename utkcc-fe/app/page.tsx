876f4a8f5fbaa5bb4d7e4610c7f2a15050e8c2ed
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-07-08 14:54:38 +0900

    feat/WIP-slide-optimization
    failure to use intersection observer API

--- utkcc-fe/app/page.tsx
+++ utkcc-fe/app/page.tsx
@@ -1,42 +1,36 @@
 'use client';
 
 import Image from 'next/image';
-import { useState, useEffect, CSSProperties } from 'react';
-
-// @ts-ignore
-// const bezierEasing: = (await import('https://cdn.skypack.dev/pin/bezier-easing@v2.1.0-ELIKFSyR7ljg13TI3nit/mode=imports/optimized/bezier-easing.js')).default
-
-interface animationProperties {
-  top: number;
-  bottom: number;
-  styleAttr: string;
-  topVal: number;
-  bottomVal: number;
-}
+import './page.css';
+import { useEffect, CSSProperties, useRef } from 'react';
 
 export default function Home() {
-  const [currentPos, setPos] = useState(0);
-
-  const updatePos = () => {
-    setPos(window.scrollY + window.innerHeight / 2);
-    console.log(currentPos);
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', updatePos, { passive: true });
-    return () => {
-      window.removeEventListener('scroll', updatePos);
-    };
-  });
-
   return (
     <div style={{ height: '7000px' }}>
-      {/* sticky container */}
-      <div className="sticky h-screen top-0">
-        {/* sticky */}
-        <div className="relative flex items-center justify-center w-full h-full">
-          {/* slide container */}
-          <Slide
+      <div className="w-full h-full">
+        {/* <div
+          className="absolute w-full"
+          style={{ top: '60vh', height: '60vh' }}
+        ></div> */}
+        <Slide
+          animations={[
+            {
+              top: '90vh',
+              bottom: '400vh',
+              keyframes: 'disappear',
+            },
+          ]}
+        >
+          <Image
+            src="/icon4.png"
+            width={300}
+            height={300}
+            alt=""
+            className="w-screen h-screen object-contain scale-50"
+          />
+        </Slide>
+        {/* slide container */}
+        {/* <Slide
             currentPos={currentPos}
             animations={[
               {
@@ -49,7 +43,6 @@ export default function Home() {
             ]}
             additionalStyle="bottom-20"
           >
-            {/* 스크롤 표시 컴포넌트 */}
             <div className="text-center">아래로 스크롤하세요.</div>
           </Slide>
           <Slide
@@ -85,7 +78,6 @@ export default function Home() {
               },
             ]}
           >
-            {/* 1. 로고 */}
             <Image
               src="/icon4.png"
               width={300}
@@ -120,7 +112,6 @@ export default function Home() {
               },
             ]}
           >
-            {/* 2. KCC 첫 텍스트 */}
             <div className="flex flex-col">
               <div className="font-bold text-4xl text-kcc-theme">UTKCC</div>
               <div className="mt-4 font-semibold text-gray-600">
@@ -154,7 +145,6 @@ export default function Home() {
               },
             ]}
           >
-            {/* 3. KCC 두번째 텍스트 */}
             <div className="font-semibold text-xl text-gray-600">
               Be part of a
               <br /> <span className="text-kcc-theme">professional</span>{' '}
@@ -235,82 +225,82 @@ export default function Home() {
               <br />
               유티는 분명 더 즐거울거에요.
             </div>
-          </Slide>
-        </div>
+          </Slide> */}
       </div>
     </div>
   );
 }
 
 /**
+ * 하나의 애니메이션/효과를 설명하는 인터페이스.
+ */
+interface animationProperties {
+  keyframes: string;
+  top: number | string;
+  bottom: number | string;
+}
+
+/**
  * 스크롤해서 등장하는 컴포넌트 한개.
- * 여러개의 `animation` 을 배열로 받으며, 현재 스크롤 값에 따라 하나의 합쳐진 스타일 객체가 적용된다.
+ * 여러개의 `animation` 을 배열로 받으며,
+ * `style` 객체의
  */
 function Slide({
   children,
   animations,
-  currentPos,
-  additionalStyle,
 }: {
   children: React.ReactNode;
   animations: animationProperties[];
-  currentPos: number;
-  additionalStyle?: string;
 }) {
+  // array that tracks each and every animation div element
+  const observedTargets = useRef<Array<HTMLDivElement | null>>([]);
+  const appliedTarget = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // observer는 하나만 있어도 됨.
+    // 초기화 하는 시점에
+    let observer: IntersectionObserver;
+    if (observedTargets) {
+      observer = new IntersectionObserver(e => {
+        e.forEach((aniDiv, index) => {
+          if (aniDiv.isIntersecting) {
+            // style 적용
+            console.log(aniDiv.intersectionRatio);
+            const { keyframes } = animations[index];
+            if (appliedTarget.current) {
+              const styleObj = appliedTarget.current.style;
+              styleObj.display = 'block';
+              styleObj.animation = `${keyframes} 1s linear infinite`;
+              styleObj.animationIterationCount = '1';
+              styleObj.animationPlayState = 'paused';
+              styleObj.animationDelay = `calc(var(${aniDiv.intersectionRatio})) * -1s`;
+            }
+          }
+        });
+      });
+      observedTargets.current.forEach(aniDiv =>
+        observer.observe(aniDiv as Element),
+      );
+    }
+  }, [observedTargets, animations]);
+
   return (
-    <div
-      className={`absolute z-0 w-full text-center ${
-        additionalStyle ? additionalStyle : ''
-      }`}
-      style={mergeAnimationObjects(animations, currentPos)}
-    >
-      {children}
+    <div>
+      <div className="fixed z-0 hidden" ref={appliedTarget}>
+        {children}
+      </div>
+      {animations.map((ani, i) => (
+        <div
+          key={i}
+          className="absolute w-full"
+          style={{
+            top: ani.top,
+            height: ani.bottom,
+            backgroundColor: 'transparent',
+          }}
+          ref={el => (observedTargets.current[i] = el)}
+        />
+      ))}
     </div>
   );
-}
-
-/**
- * 현재 스크롤값에 따라 활성화되어있는 모든 CSSProperties를 하나의 객체에 옮겨담는 함수
- */
-function mergeAnimationObjects(
-  animations: animationProperties[],
-  currentPos: number,
-): CSSProperties {
-  const mergedCSSObject = animations.reduce(
-    (prevObj: CSSProperties, currentStyleObj) =>
-      isInRange(currentPos, currentStyleObj.top, currentStyleObj.bottom)
-        ? { ...prevObj, ...createAnimationObject(currentStyleObj, currentPos) }
-        : prevObj,
-    {},
-  );
-
-  return Object.keys(mergedCSSObject).length == 0 // == {} ?
-    ? { display: 'none' }
-    : mergedCSSObject;
-}
-
-/**
- * 인자들에 따른 현재 애니메이션의 진행률을 계산해서 `CSSProperties` 타입 오브젝트를 리턴하는 함수.
- */
-function createAnimationObject(
-  animation: animationProperties,
-  currentPos: number,
-): CSSProperties {
-  const { top, bottom, styleAttr, topVal, bottomVal } = animation;
-  const ratio = (currentPos - top) / (bottom - top);
-  const currentVal = topVal + (bottomVal - topVal) * ratio;
-  return {
-    [styleAttr]: currentVal,
-  };
-}
-
-const ease = 'cubicBezier(.25, .1, .25, 1)';
-const easeIn = 'cubicBezier(.38, .01, .78, .13)';
-const midSlow = 'cubicBezier(0, .7, 1, .3)';
-
-/**
- * 해당 숫자가 `top`과 `bottom` 사이에 있는지 확인하는 함수
- */
-function isInRange(num: number, top: number, bottom: number) {
-  return num >= top && num <= bottom;
 }
