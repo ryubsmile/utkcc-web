785ca1b105f21a2c15f9c8b14d9a96d846fab29f
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-08-12 17:23:08 +0900

    intro optimized

--- utkcc-fe/app/page.tsx
+++ utkcc-fe/app/page.tsx
@@ -5,22 +5,16 @@ import { useEffect, CSSProperties, useRef } from 'react';
 
 export default function Home() {
   return (
-    <div style={{ height: '7000px' }}>
+    <div style={{ height: 'max-content' }}>
       <div>
+        <LogoSlide duration={500} />
         <Slide
           duration={500}
           transitions={[
             {
               top: '150vh',
-              bottom: '300vh',
-              startOpacityVal: '1',
-              endOpacityVal: '0.2',
-            },
-            {
-              top: '300vh',
-              bottom: '410vh',
-              startOpacityVal: '0.2',
-              endOpacityVal: '0',
+              height: '330vh',
+              opacityTo: '0.2',
             },
           ]}
         >
@@ -36,16 +30,67 @@ export default function Home() {
           duration={500}
           transitions={[
             {
-              top: '300vh',
-              bottom: '410vh',
-              startOpacityVal: '0',
-              endOpacityVal: '1',
+              top: '170vh',
+              height: '50vh',
+              opacityTo: '1',
             },
+          ]}
+        >
+          <div className="font-semibold text-xl">
+            학업성취와 커리어의 첫 시작점 <br />
+            모두 이 곳에서 시작하세요. <br />
+            <span className="text-transparent">
+              이제껏 경험 못 했던 한인경영동아리, <br /> <span>UTKCC</span>와
+              함께라면 당신의 <br />
+              유티는 분명 더 즐거울거에요.
+            </span>
+          </div>
+        </Slide>
+        <Slide
+          duration={500}
+          transitions={[
             {
-              top: '410vh',
-              bottom: '600vh',
-              startOpacityVal: '1',
-              endOpacityVal: '0',
+              top: '210vh',
+              height: '10vh',
+              opacityTo: '1',
+            },
+          ]}
+        >
+          <div className="font-semibold text-xl">
+            <span className="text-transparent">
+              학업성취와 커리어의 첫 시작점 <br />
+              모두 이 곳에서 시작하세요.{' '}
+            </span>
+            <br />
+            이제껏 경험 못 했던 한인경영동아리, <br />{' '}
+            <span className="text-kcc-theme">UTKCC</span>와 함께라면 당신의{' '}
+            <br />
+            유티는 분명 더 즐거울거에요.
+          </div>
+        </Slide>
+        <Slide
+          duration={500}
+          transitions={[
+            {
+              top: '340vh',
+              height: '10vh',
+              opacityTo: '1',
+            },
+          ]}
+        >
+          <div className="font-semibold text-xl text-gray-600">
+            Be part of a
+            <br /> <span className="text-kcc-theme">professional</span>{' '}
+            community.
+          </div>
+        </Slide>
+        <Slide
+          duration={500}
+          transitions={[
+            {
+              top: '470vh',
+              height: '10vh',
+              opacityTo: '1',
             },
           ]}
         >
@@ -56,192 +101,30 @@ export default function Home() {
             </div>
           </div>
         </Slide>
-        {/* <div
-          className="absolute w-full"
-          style={{ top: '60vh', height: '60vh' }}
-        ></div> */}
-        {/* slide container */}
-        {/* <Slide
-            currentPos={currentPos}
-            transitions={[
-              {
-                top: 0,
-                bottom: 1500,
-                styleAttr: 'opacity',
-                topVal: 1,
-                bottomVal: 0,
-              },
-            ]}
-            additionalStyle="bottom-20"
-          >
-            <div className="text-center">아래로 스크롤하세요.</div>
-          </Slide>
-          <Slide
-            currentPos={currentPos}
-            transitions={[
-              {
-                top: 0,
-                bottom: 500,
-                styleAttr: 'opacity',
-                topVal: 1,
-                bottomVal: 1,
-              },
-              {
-                top: 500,
-                bottom: 1000,
-                styleAttr: 'opacity',
-                topVal: 1,
-                bottomVal: 0.2,
-              },
-              {
-                top: 1000,
-                bottom: 4000,
-                styleAttr: 'opacity',
-                topVal: 0.2,
-                bottomVal: 0.2,
-              },
-              {
-                top: 4000,
-                bottom: 4500,
-                styleAttr: 'opacity',
-                topVal: 0.2,
-                bottomVal: 0,
-              },
-            ]}
-          >
-            <Image
-              src="/icon4.png"
-              width={300}
-              height={300}
-              alt=""
-              className="w-screen h-screen object-contain scale-50"
-            />
-          </Slide>
-          <Slide
-            currentPos={currentPos}
-            transitions={[
-              {
-                top: 1000,
-                bottom: 1500,
-                styleAttr: 'opacity',
-                topVal: 0,
-                bottomVal: 1,
-              },
-              {
-                top: 1500,
-                bottom: 2200,
-                styleAttr: 'opacity',
-                topVal: 1,
-                bottomVal: 1,
-              },
-              {
-                top: 2200,
-                bottom: 2500,
-                styleAttr: 'opacity',
-                topVal: 1,
-                bottomVal: 0,
-              },
-            ]}
-          >
-            <div className="flex flex-col">
-              <div className="font-bold text-4xl text-kcc-theme">UTKCC</div>
-              <div className="mt-4 font-semibold text-gray-600">
-                University of Toronto <br /> Korean Commerce Community
-              </div>
-            </div>
-          </Slide>
-          <Slide
-            currentPos={currentPos}
-            transitions={[
-              {
-                top: 2500,
-                bottom: 3000,
-                styleAttr: 'opacity',
-                topVal: 0,
-                bottomVal: 1,
-              },
-              {
-                top: 3000,
-                bottom: 3700,
-                styleAttr: 'opacity',
-                topVal: 1,
-                bottomVal: 1,
-              },
-              {
-                top: 3700,
-                bottom: 4000,
-                styleAttr: 'opacity',
-                topVal: 1,
-                bottomVal: 0,
-              },
-            ]}
-          >
-            <div className="font-semibold text-xl text-gray-600">
-              Be part of a
-              <br /> <span className="text-kcc-theme">professional</span>{' '}
-              community.
-            </div>
-          </Slide>
-
-          <Slide
-            currentPos={currentPos}
-            transitions={[
-              {
-                top: 4000,
-                bottom: 4300,
-                styleAttr: 'opacity',
-                topVal: 0,
-                bottomVal: 1,
-              },
-              {
-                top: 4300,
-                bottom: 6000,
-                styleAttr: 'opacity',
-                topVal: 1,
-                bottomVal: 1,
-              },
-              // {
-              //   top: 5500,
-              //   bottom: 5800,
-              //   styleAttr: 'opacity',
-              //   topVal: 1,
-              //   bottomVal: 0,
-              // },
-            ]}
-          >
-            <div className="font-semibold text-xl">
-              학업성취와 커리어의 첫 시작점 <br />
-              모두 이 곳에서 시작하세요. <br />
-              <span className="text-transparent">
-                이제껏 경험 못 했던 한인경영동아리, <br /> <span>UTKCC</span>와
-                함께라면 당신의 <br />
-                유티는 분명 더 즐거울거에요.
-              </span>
-            </div>
-          </Slide>
+        {/*
           <Slide
             currentPos={currentPos}
             transitions={[
               {
                 top: 5000,
-                bottom: 5300,
+                height: 5300,
                 styleAttr: 'opacity',
                 topVal: 0,
-                bottomVal: 1,
+                heightVal: 1,
               },
               {
                 top: 5300,
-                bottom: 6000,
+                height: 6000,
                 styleAttr: 'opacity',
                 topVal: 1,
-                bottomVal: 1,
+                heightVal: 1,
               },
               // {
               //   top: 5500,
-              //   bottom: 5800,
+              //   height: 5800,
               //   styleAttr: 'opacity',
               //   topVal: 1,
-              //   bottomVal: 0,
+              //   heightVal: 0,
               // },
             ]}
           >
@@ -267,9 +150,79 @@ export default function Home() {
  */
 interface transitionInfo {
   top: number | string;
-  bottom: number | string;
-  startOpacityVal: string;
-  endOpacityVal: string;
+  height: number | string;
+  opacityTo: string;
+}
+
+/**
+ * 로고 슬라이드 컴포넌트.
+ * 위로 스크롤하면 보이고, 아래로 스크롤하면 사라진다.
+ */
+function LogoSlide({ duration }: { duration: number }) {
+  const appliedTarget = useRef<HTMLDivElement>(null);
+  const observedTarget = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    let observer: IntersectionObserver;
+    let previousY = 0;
+    let previousRatio = 0;
+
+    const handleIntersect = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach(entry => {
+        const currentY = entry.boundingClientRect.y;
+        const currentRatio = entry.intersectionRatio;
+        const isIntersecting = entry.isIntersecting;
+
+        // Scrolling down/up
+        if (appliedTarget.current) {
+          if (currentY < previousY) {
+            if (!(currentRatio > previousRatio && isIntersecting)) {
+              // none
+              appliedTarget.current.style.opacity = '0';
+            } else {
+              appliedTarget.current.style.opacity = '0';
+            }
+          } else if (currentY > previousY && isIntersecting) {
+            if (currentRatio < previousRatio) {
+              // none
+              appliedTarget.current.style.opacity = '1.0';
+            } else {
+              appliedTarget.current.style.opacity = '1.0';
+            }
+          }
+        }
+
+        previousY = currentY;
+        previousRatio = currentRatio;
+      });
+    };
+
+    observer = new IntersectionObserver(handleIntersect);
+    observer.observe(observedTarget.current as Element);
+  });
+
+  return (
+    <>
+      <div className="w-full fixed h-full flex text-center items-center">
+        <div
+          className={`absolute z-0 w-full ease-in transition duration-${duration} z-50`}
+          ref={appliedTarget}
+        >
+          <Image
+            src="/icon4.png"
+            width={300}
+            height={300}
+            alt=""
+            className="w-screen h-screen object-contain scale-50"
+          />
+        </div>
+      </div>
+      <div
+        className="absolute w-full top-[0px] h-[50vh] bg-transparent"
+        ref={observedTarget}
+      ></div>
+    </>
+  );
 }
 
 /**
@@ -291,22 +244,36 @@ function Slide({
   const appliedTarget = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (appliedTarget.current) {
-      appliedTarget.current.style.opacity = transitions[0]?.startOpacityVal;
-      console.log(transitions);
-    }
-
     let observer: IntersectionObserver;
-    if (observedTargets) {
-      observer = new IntersectionObserver(e => {
-        e.forEach((aniDiv, index) => {
-          if (appliedTarget.current) {
-            // style 적용
-            appliedTarget.current.style.opacity = aniDiv.isIntersecting
-              ? transitions[index].endOpacityVal
-              : '';
+
+    // 요소가 화면에 등장할시 실행되는 함수
+    const handleIntersect = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((aniDiv, index) => {
+        if (appliedTarget.current) {
+          // style 적용
+          console.log(
+            aniDiv.isIntersecting,
+            transitions[index].opacityTo,
+            observedTargets.current,
+          );
+          if (aniDiv.isIntersecting) {
+            appliedTarget.current.style.opacity = transitions[index].opacityTo;
+          } else {
+            appliedTarget.current.style.opacity = '0';
           }
-        });
+        }
+      });
+    };
+
+    // threshold 배열 생성함수
+    const thresholdArray = (steps: number) =>
+      Array(steps + 1)
+        .fill(0)
+        .map((_, index) => index / steps || 0);
+
+    if (observedTargets) {
+      observer = new IntersectionObserver(handleIntersect, {
+        threshold: thresholdArray(20),
       });
       observedTargets.current.forEach(aniDiv =>
         observer.observe(aniDiv as Element),
@@ -327,39 +294,14 @@ function Slide({
       {transitions.map((trans, i) => (
         <div
           key={i}
-          className="absolute w-full"
+          className="absolute w-full bg-gray-200 opacity-40"
           style={{
             top: trans.top,
-            height: trans.bottom,
-            backgroundColor: 'transparent',
+            height: trans.height,
           }}
           ref={el => (observedTargets.current[i] = el)}
         />
       ))}
-      {/* <div
-        className="absolute w-screen top-[200vh] h-[400vh] bg-black opacity-50 z-10"
-        ref={}
-      ></div> */}
-
-      {/* <div>
-      <div className="fixed z-0 hidden" ref={appliedTarget}>
-        {children}
-      </div>
-      {transitions.map((ani, i) => (
-    <>
-      <div className="w-full fixed h-full flex text-center items-center">
-        <div
-          key={i}
-          className="absolute w-full"
-          style={{
-            top: ani.top,
-            height: ani.bottom,
-            backgroundColor: 'transparent',
-          }}
-          ref={el => (observedTargets.current[i] = el)}
-        />
-      ))}
-    </div> */}
     </>
   );
 }
