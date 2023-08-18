3e442cc66e9733a8c85f570524b8409d8e171426
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-08-18 15:34:47 +0900

    chore/added variables to footers
    emails are now variables

--- utkcc-fe/app/layout.tsx
+++ utkcc-fe/app/layout.tsx
@@ -47,6 +47,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const kccEmail = 'koreancommerce@gmail.com';
+  const presEmail = 'seohyun.kang@mail.utoronto.ca';
+  const vicePresEmail = 'suye.han@mail.utoronto.ca';
+  const erDirectorEmail = 'yujin.shim@mail.utoronto.ca';
+
   return (
     <html lang="en" className={gMarket.className}>
       <body
@@ -62,10 +67,10 @@ export default function RootLayout({
           <div className="flex flex-col min-h-[20vh] mt-[10vh] p-8 bg-kcc-theme text-white font-normal">
             <div className="text-center">Contact Information</div>
             <div className="flex flex-col my-3 text-xs">
-              <div>General Inquiries: koreancommerce@gmail.com</div>
-              <div>President: seohyun.kang@mail.utoronto.ca</div>
-              <div>Vice-president: suye.han@mail.utoronto.ca</div>
-              <div>Sponsor Inquiries: yujin.shim@mail.utoronto.ca</div>
+              <div>General Inquiries: {kccEmail}</div>
+              <div>President: {presEmail}</div>
+              <div>Vice-president: {vicePresEmail}</div>
+              <div>Sponsor Inquiries: {erDirectorEmail}</div>
               <div className="pt-8 pb-2 opacity-50">
                 Designed by Hyunjun You, <br /> Created by Jaehyuk Ryu and
                 Jingeon Kim
