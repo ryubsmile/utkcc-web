d1aca71b76e1a15b35e5ea6d3b38e192e7ee4a5d
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-08-23 18:56:29 +0900

    encapsulated events intro

--- utkcc-fe/app/(homepage)/page.tsx
+++ utkcc-fe/app/(homepage)/page.tsx
@@ -1,42 +1,20 @@
-import Image from 'next/image';
 import About from '../(subpages)/about/page';
 import Executives from '../(subpages)/executives/page';
 import Sponsors from '../(subpages)/sponsors/page';
 import Resources from '../(subpages)/resources/page';
 import Newsletter from '../(subpages)/newsletter/page';
 import Slides from '@/components/slides';
-import PageIntro from '@/components/pageIntro';
-import LearnMoreLink from '@/components/learnMoreLink';
-import eventsImage from '/public/assets/events-image.png';
+import EventsHomeIntro from '../(subpages)/events/eventsHomeIntro';
 
 export default function Home() {
   return (
     <div className="flex-col">
       <Slides />
-      <div className="px-8 lg:px-32 z-50 flex flex-col gap-y-[20vh]">
+      <div className="px-8 lg:px-32 z-50 flex flex-col gap-y-[15vh] mb-[10vh]">
         {/* about intro */}
         <About />
         {/* events intro */}
-        <PageIntro
-          pageName="events"
-          pageSlogan={
-            <>
-              때론 아카데믹하게, <br /> 때론 프로페셔널하게, <br /> 때론 즐겁게
-            </>
-          }
-          pageExp={
-            <>
-              학업성취를 위한 튜토리얼, 관심 있는 산업에 종사하시는 alumni분들을
-              만나볼 수 있는 네트워킹 세션, 스트레스를 시원하게 날릴 소셜
-              이벤트.
-              <span className="my-3 w-full block" />
-              모두 UTKCC에서 만나보세요.
-              <LearnMoreLink name="events" />
-            </>
-          }
-        >
-          <Image alt="events image" className="h-fit" src={eventsImage} />
-        </PageIntro>
+        <EventsHomeIntro />
         {/* exec intro */}
         <Executives />
         {/* sponsors intro */}
---
+++ utkcc-fe/app/(subpages)/events/eventsHomeIntro.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import PageIntro from '@/components/pageIntro';
+import LearnMoreLink from '@/components/learnMoreLink';
+import eventsImage from '/public/assets/events-image.png';
+
+export default function EventsHomeIntro() {
+  return (
+    <PageIntro
+      pageName="events"
+      pageSlogan={
+        <>
+          때론 아카데믹하게, <br /> 때론 프로페셔널하게, <br /> 때론 즐겁게
+        </>
+      }
+      pageExp={
+        <>
+          학업성취를 위한 튜토리얼, 관심 있는 산업에 종사하시는 alumni분들을
+          만나볼 수 있는 네트워킹 세션, 스트레스를 시원하게 날릴 소셜 이벤트.
+          <span className="my-3 w-full block" />
+          모두 UTKCC에서 만나보세요.
+          <LearnMoreLink name="events" />
+        </>
+      }
+    >
+      <Image
+        alt="events image"
+        className="w-auto h-[40vh] object-contain"
+        src={eventsImage}
+      />
+    </PageIntro>
+  );
+}
