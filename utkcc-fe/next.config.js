1b39176245cd90fa2d994cdcac51ec87510e9993
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-08-28 16:57:32 +0900

    fix/image src set is reduced.

--- utkcc-fe/next.config.js
+++ utkcc-fe/next.config.js
@@ -1,8 +1,14 @@
+// @ts-check
+// import withPlaiceholder from '@plaiceholder/next';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
   },
 };
 
+// export default withPlaiceholder(nextConfig);
 module.exports = nextConfig;
