e31e7b9e02d5b7e03973f72b8a25942fc7ba8c46
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-09-04 21:41:29 +0900

    changed css name to add intuitition

--- utkcc-fe/components/navbar.tsx
+++ utkcc-fe/components/navbar.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState, useEffect, useRef } from 'react';
-import './navbarMenuButton.css';
+import './navbar.css';
 import { handleScroll } from './utils';
 import SmallLogoImage from '/public/assets/images/logo-nav.png';
 
