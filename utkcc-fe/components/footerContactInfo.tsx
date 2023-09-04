db5dd703d7a895284e1e7be9666e458918652ed0
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-09-04 21:21:51 +0900

    removed unused imports and csr

--- utkcc-fe/components/footerContactInfo.tsx
+++ utkcc-fe/components/footerContactInfo.tsx
@@ -1,29 +1,19 @@
-'use client';
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
 
 export default function FooterContactInfo() {
-  const [mounted, setMounted] = useState(false);
-
   const kccEmail = 'koreancommerce@gmail.com';
   const presEmail = 'seohyun.kang@mail.utoronto.ca';
   const vicePresEmail = 'suye.han@mail.utoronto.ca';
   const erDirectorEmail = 'yujin.shim@mail.utoronto.ca';
 
-  const loadingText = 'loading...';
-
-  useEffect(() => {
-    setMounted(true);
-  }, [setMounted]);
-
   return (
     <div className="flex flex-col min-h-[20vh] p-8 lg:px-32 bg-kcc-theme text-white font-normal">
       <div className="text-center">Contact Information</div>
       <div className="flex flex-col my-3 text-xs">
-        <div>General Inquiries: {mounted ? kccEmail : loadingText}</div>
-        <div>President: {mounted ? presEmail : loadingText}</div>
-        <div>Vice-president: {mounted ? vicePresEmail : loadingText}</div>
-        <div>Sponsor Inquiries: {mounted ? erDirectorEmail : loadingText}</div>
+        <div>General Inquiries: {kccEmail}</div>
+        <div>President: {presEmail}</div>
+        <div>Vice-president: {vicePresEmail}</div>
+        <div>Sponsor Inquiries: {erDirectorEmail}</div>
         <div className="pt-8 pb-2 opacity-30">
           Designed by{' '}
           <Link href="https://www.instagram.com/hyunjun2003/" target="_blank">
--- utkcc-fe/components/pageIntro.tsx
+++ utkcc-fe/components/pageIntro.tsx
@@ -1,6 +1,3 @@
-'use client';
-import { useState, useEffect } from 'react';
-
 export default function PageIntro({
   children,
   pageName,
@@ -12,28 +9,20 @@ export default function PageIntro({
   pageSlogan: string | React.ReactNode;
   pageExp: string | React.ReactNode;
 }) {
-  const [name, setName] = useState('');
-  const [slogan, setSlogan] = useState<React.ReactNode | string | null>(null);
-  const [exp, setExp] = useState<React.ReactNode | string | null>(null);
-
-  useEffect(() => {
-    setName(pageName);
-    setSlogan(pageSlogan);
-    setExp(pageExp);
-  }, [pageName, pageSlogan, pageExp]);
-
   return (
     <article className="h-auto w-full lg:pt-0 lg:grid grid-cols-right-tilt-twin-columns gap-x-[5vw] grid-rows-3-auto-rows grid-flow-col-dense items-end relative">
       <div id={pageName} className="absolute -top-32"></div>
       {/* section intro title */}
-      <div className="text-kcc-theme font-bold capitalize self-end">{name}</div>
+      <div className="text-kcc-theme font-bold text-xl capitalize self-end">
+        {pageName}
+      </div>
       {/* section intro slogans */}
-      <div className="text-black font-normal text-2xl my-6">{slogan}</div>
+      <div className="text-black font-normal text-2xl my-6">{pageSlogan}</div>
       <div className="max-w-full w-full h-full flex items-center justify-center mx-auto row-span-2 row-start-2">
         {children}
       </div>
       <p className="break-keep hyphens-auto font-normal my-6 lg:mt-0 text-kcc-gray self-start">
-        {exp}
+        {pageExp}
       </p>
     </article>
   );
